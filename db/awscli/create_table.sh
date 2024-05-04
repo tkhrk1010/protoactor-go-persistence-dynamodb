8dc8c5a5bed03fdf84d1659c7176c9edb773ed9b
tkhrk1010 <>
2024-05-04 15:00:23 +0900

    mod: create_table sh

--- db/awscli/create_table.sh
+++ db/awscli/create_table.sh
@@ -4,7 +4,22 @@
 # localhostじゃないことに注意。
 docker-compose exec awscli aws dynamodb create-table \
     --endpoint-url=http://host.docker.internal:4566 \
-    --table-name MyTable \
-    --attribute-definitions AttributeName=ID,AttributeType=S \
-    --key-schema AttributeName=ID,KeyType=HASH \
+    --table-name journal \
+    --attribute-definitions \
+        AttributeName=actorName,AttributeType=S \
+        AttributeName=eventIndex,AttributeType=N \
+    --key-schema \
+        AttributeName=actorName,KeyType=HASH \
+        AttributeName=eventIndex,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
+
+docker-compose exec awscli aws dynamodb create-table \
+    --endpoint-url=http://host.docker.internal:4566 \
+    --table-name snapshot \
+    --attribute-definitions \
+        AttributeName=actorName,AttributeType=S \
+        AttributeName=eventIndex,AttributeType=N \
+    --key-schema \
+        AttributeName=actorName,KeyType=HASH \
+        AttributeName=eventIndex,KeyType=RANGE \
     --provisioned-throughput ReadCapacityUnits=1,WriteCapacityUnits=1
--- main.go
+++ main.go
@@ -19,7 +19,6 @@ import (
 )
 
 func main() {
-	// TODO: なぜかrerunしたらDynamoDBのeventIndexかなにかでerrorが出る
 	log.Printf("start")
 
 	//
